757dfb8f4a25e3cfac5444e3be6c3e741cf8b841
Abdullah-Jawahir <mjabdullah33@gmail.com>
2023-12-17 08:48:38 +0530

    added the resposiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,25 @@
 import bgDesktopDarkImg from '../assets/bg-desktop-dark.jpg';
 import bgDesktopLightImg from '../assets/bg-desktop-light.jpg';
+import bgMobileDarkImg from '../assets/bg-mobile-dark.jpg';
+import bgMobileLightImg from '../assets/bg-mobile-light.jpg';
 import PropTypes from 'prop-types';
 
 export const Banner = ({ isLightTheme }) => {
 
-    const bannerStyles = {
+    const curWinWidth = window.outerWidth;
+    let bannerStyles = {
 
-        backgroundImage: `url(${isLightTheme ? bgDesktopLightImg : bgDesktopDarkImg})`
+        backgroundImage: '',
     }
+
+
+    if (curWinWidth <= 430) {
+        bannerStyles.backgroundImage = `url(${isLightTheme ? bgMobileLightImg : bgMobileDarkImg})`
+
+    } else {
+        bannerStyles.backgroundImage = `url(${isLightTheme ? bgDesktopLightImg : bgDesktopDarkImg})`
+    }
+
 
 
     return (
